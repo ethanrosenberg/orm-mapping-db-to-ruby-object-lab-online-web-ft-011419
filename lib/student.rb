4cc0d4dc25f7f4152d3e5472f1f07671b64e28ea
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-19 19:43:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -100,7 +100,16 @@ class Student
   end
 
   def self.first_student_in_grade_10
-    self.first_X_students_in_grade_10(1)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+      LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, 10, limit).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
 
